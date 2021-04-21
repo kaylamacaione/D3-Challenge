cec529d65052266672f470342ed323dbf0d2bbd8
kaylamacaione <kmacaione@labelinsight.com>
2021-04-20 19:34:17 -0500

    organize files

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,1 +1,31 @@
-// @TODO: YOUR CODE HERE!
+// define variables and margins
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 50
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+
+//create SVG wrapper
+var svg = d3
+  .select("body")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+
+//import data
+d3.csv("data.csv").then(function(dataSet) {
+    console.log(dataSet)
+});
+
